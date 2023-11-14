f2ac278c3ee41cf2b4e85f9ba66be0d32192ebea
Nando <fromavaz@emeal.nttdata.com>
2023-11-14 18:03:24 +0100

    generated_code folder was commited from iBDD

--- generated_code/SearchInGooglePage.java
+++ generated_code/SearchInGooglePage.java
@@ -6,9 +6,9 @@ import org.openqa.selenium.WebDriver;
 
 
 public class SearchInGooglePage{ 
-	public static WebDriver driver;
+	public static By Search374 = ByScrap.textInside("Search");
 
-	public static By Search206 = ByScrap.textInside("Search");
+	public static WebDriver driver;
 
 	public static By Search = ByScrap.closestFieldWithLabel("Search");
 
