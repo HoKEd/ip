7a1733e4f4f65c0f7b60b8b5b5d7646e4c174a07
Nando <fromavaz@emeal.nttdata.com>
2023-11-14 10:33:18 +0100

    generated_code folder was commited from iBDD

--- generated_code/GherkinFeatureLexerPage.java
+++ generated_code/GherkinFeatureLexerPage.java
@@ -1,12 +1,28 @@
-package com.everis.stepsdef;
+package cinnamontest.pages;
 
-import org.openqa.selenium.By;
-import com.everis.scrapping.ByScrap;
-import org.openqa.selenium.WebDriver;
+import com.nttdata.cinnamon.driver.By;
+import com.nttdata.cinnamon.driver.Find;
+import com.nttdata.cinnamon.driver.controls.Element;
+import com.nttdata.cinnamon.driver.controls.InputTextField;
+import static org.assertj.core.api.Assertions.assertThat;
 
 
 public class GherkinFeatureLexerPage{ 
-	public static WebDriver driver;
+	@Find(by = By.XPath, locator = "//body")
+	public Element formTitle;
 
+	private final String TITLE = "";
+
+
+	/** null
+	 */
+	
+	public boolean isPageDisplayed() {
+		assertThat(this.formTitle.isDisplayed())
+			                .withFailMessage("Title element not displayed!")
+			                .isTrue();
+		
+			        return this.formTitle.getText().equals(TITLE);
+	}
 
 }
