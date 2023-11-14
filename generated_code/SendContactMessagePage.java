dfdee4cf97c0f8913ff965ab1781946d057ee1a2
Nando <fromavaz@emeal.nttdata.com>
2023-11-14 16:58:06 +0100

    generated_code folder was commited from iBDD

--- generated_code/SendContactMessagePage.java
+++ generated_code/SendContactMessagePage.java
@@ -1,53 +1,28 @@
-package cinnamontest.pages;
+package com.everis.stepsdef;
 
-import com.nttdata.cinnamon.driver.By;
-import com.nttdata.cinnamon.driver.Find;
-import com.nttdata.cinnamon.driver.controls.Element;
-import com.nttdata.cinnamon.driver.controls.InputTextField;
-import static org.assertj.core.api.Assertions.assertThat;
-import com.nttdata.cinnamon.modules.selenium.controls.SelectControl;
+import org.openqa.selenium.By;
+import com.everis.scrapping.ByScrap;
+import org.openqa.selenium.WebDriver;
 
 
 public class SendContactMessagePage{ 
-	@Find(by = By.Name, locator="Company")
-	public InputTextField Company;
+	public static By SendMessage = ByScrap.textInside("SendMessage");
 
-	@Find(by = By.Name, locator="Your message field")
-	public InputTextField Yourmessagefield;
+	public static WebDriver driver;
 
-	@Find(by = By.Name, locator="First Name")
-	public InputTextField FirstName;
+	public static By Yourmessagefield = ByScrap.closestFieldWithLabel("Yourmessagefield");
 
-	@Find(by = By.Name, locator="Phone")
-	public InputTextField Phone;
+	public static By FirstName = ByScrap.closestFieldWithLabel("FirstName");
 
-	@Find(by = By.Name, locator="Select field Label")
-	public SelectControl SelectfieldLabel;
+	public static By LastName = ByScrap.closestFieldWithLabel("LastName");
 
-	@Find(by = By.XPath, locator = "//body")
-	public Element formTitle;
+	public static By Phone = ByScrap.closestFieldWithLabel("Phone");
 
-	@Find(by = By.Name, locator="Last Name")
-	public InputTextField LastName;
+	public static By SelectfieldLabel = ByScrap.closestSelectWithLabel("SelectfieldLabel");
 
-	@Find(by = By.Name, locator="Job")
-	public Element Job;
+	public static By Job = ByScrap.textInside("Job");
 
-	private final String TITLE = "";
+	public static By Company = ByScrap.closestFieldWithLabel("Company");
 
-	@Find(by = By.Name, locator="Send Message")
-	public Element SendMessage;
-
-
-	/** null
-	 */
-	
-	public boolean isPageDisplayed() {
-		assertThat(this.formTitle.isDisplayed())
-			                .withFailMessage("Title element not displayed!")
-			                .isTrue();
-		
-			        return this.formTitle.getText().equals(TITLE);
-	}
 
 }
