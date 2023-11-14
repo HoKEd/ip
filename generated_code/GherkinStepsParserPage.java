64735409a6e1c21de2c1d3977acf17ef0c8d1483
Nando <fromavaz@emeal.nttdata.com>
2023-11-14 11:31:31 +0100

    generated_code folder was commited from iBDD

--- generated_code/GherkinStepsParserPage.java
+++ generated_code/GherkinStepsParserPage.java
@@ -1,40 +1,12 @@
-package cinnamontest.pages;
+package com.everis.stepsdef;
 
-import com.nttdata.cinnamon.driver.By;
-import com.nttdata.cinnamon.driver.Find;
-import com.nttdata.cinnamon.driver.controls.Element;
-import com.nttdata.cinnamon.driver.controls.InputTextField;
-import static org.assertj.core.api.Assertions.assertThat;
+import org.openqa.selenium.By;
+import com.everis.scrapping.ByScrap;
+import org.openqa.selenium.WebDriver;
 
 
 public class GherkinStepsParserPage{ 
-	@Find(by = By.Name, locator="following")
-	public InputTextField following296;
+	public static WebDriver driver;
 
-	@Find(by = By.Name, locator="following")
-	public InputTextField following470;
-
-	@Find(by = By.Name, locator="line")
-	public Element line;
-
-	@Find(by = By.Name, locator="following")
-	public InputTextField following;
-
-	@Find(by = By.XPath, locator = "//body")
-	public Element formTitle;
-
-	private final String TITLE = "";
-
-
-	/** null
-	 */
-	
-	public boolean isPageDisplayed() {
-		assertThat(this.formTitle.isDisplayed())
-			                .withFailMessage("Title element not displayed!")
-			                .isTrue();
-		
-			        return this.formTitle.getText().equals(TITLE);
-	}
 
 }
