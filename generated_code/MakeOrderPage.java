2ee1c1d1cf06fdea83fa921e2e5e653e98bbf147
Nando <fromavaz@emeal.nttdata.com>
2023-11-14 16:39:26 +0100

    generated_code folder was commited from iBDD

--- generated_code/MakeOrderPage.java
+++ generated_code/MakeOrderPage.java
@@ -1,14 +1,31 @@
-package com.everis.stepsdef;
+package cinnamontest.pages;
 
-import org.openqa.selenium.By;
-import com.everis.scrapping.ByScrap;
-import org.openqa.selenium.WebDriver;
+import com.nttdata.cinnamon.driver.By;
+import com.nttdata.cinnamon.driver.Find;
+import com.nttdata.cinnamon.driver.controls.Element;
+import com.nttdata.cinnamon.driver.controls.InputTextField;
+import static org.assertj.core.api.Assertions.assertThat;
 
 
 public class MakeOrderPage{ 
-	public static WebDriver driver;
+	@Find(by = By.XPath, locator = "//body")
+	public Element formTitle;
 
-	public static By representation = ByScrap.textInside("representation");
+	private final String TITLE = "";
 
+	@Find(by = By.Name, locator="representation")
+	public Element representation;
+
+
+	/** null
+	 */
+	
+	public boolean isPageDisplayed() {
+		assertThat(this.formTitle.isDisplayed())
+			                .withFailMessage("Title element not displayed!")
+			                .isTrue();
+		
+			        return this.formTitle.getText().equals(TITLE);
+	}
 
 }
