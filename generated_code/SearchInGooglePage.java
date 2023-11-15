3631f6b560dea7e231f0f15fab424666cb95fe82
Nando <fromavaz@emeal.nttdata.com>
2023-11-15 17:44:40 +0100

    generated_code folder was commited from iBDD

--- generated_code/SearchInGooglePage.java
+++ generated_code/SearchInGooglePage.java
@@ -1,28 +1,16 @@
-package cinnamontest.pages;
+package com.everis.stepsdef;
 
-import com.nttdata.cinnamon.driver.By;
-import com.nttdata.cinnamon.driver.Find;
-import com.nttdata.cinnamon.driver.controls.Element;
-import com.nttdata.cinnamon.driver.controls.InputTextField;
-import static org.assertj.core.api.Assertions.assertThat;
+import org.openqa.selenium.By;
+import com.everis.scrapping.ByScrap;
+import org.openqa.selenium.WebDriver;
 
 
 public class SearchInGooglePage{ 
-	@Find(by = By.XPath, locator = "//body")
-	public Element formTitle;
+	public static By Search985 = ByScrap.textInside("Search");
 
-	private final String TITLE = "";
+	public static WebDriver driver;
 
+	public static By Search = ByScrap.closestFieldWithLabel("Search");
 
-	/** null
-	 */
-	
-	public boolean isPageDisplayed() {
-		assertThat(this.formTitle.isDisplayed())
-			                .withFailMessage("Title element not displayed!")
-			                .isTrue();
-		
-			        return this.formTitle.getText().equals(TITLE);
-	}
 
 }
